25522241dd3cfbb02247d3974b27da3fadbd5e9b
avinash38143 <avinash38143@gmail.com>
2024-01-24 20:14:34 +0530

    Styling enhancements.

--- src/components/HeroComponents/HeroComponent.jsx
+++ src/components/HeroComponents/HeroComponent.jsx
@@ -8,7 +8,7 @@ const HeroComponent = () => {
         <div className="section-hero-data">
           <p className="hero-top-data">THIS IS ME</p>
           <h1 className="hero-heading">All dataset and Your File</h1>
-          <p className="hero-para text-info">
+          <p className="hero-para ">
           Welcome to "FireFiles", where the seamless integration of React, Redux, and Firebase transforms file management into an intuitive experience. Unleash the power of dynamic organization with our user-friendly interface, enhanced by Redux's responsive functionality. With fortified security measures courtesy of Firebase, your files are protected while offering anytime, anywhere access. Elevate collaboration to new heights as you effortlessly share and collaborate on documents, images, and more in real-time. FireFiles isn't just a file management system; it's a dynamic ecosystem designed to redefine the way you interact with and organize your digital assets. Embrace the future of productivity with FireFiles – where innovation meets efficiency.
           </p>
         </div>
